84d85aea1eb214a2d6ee9bcf042d153cb97d4ec3
Billy <cole.billys@gmail.com>
2019-03-28 14:03:16 -0700

    make piece appear, update board

--- connect4.js
+++ connect4.js
@@ -10,10 +10,11 @@
  * 
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+const WIDTH = 7;
+const HEIGHT = 6;
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
+let turnCounter = 0;
 
 
 /********************************************************************************/
@@ -23,10 +24,9 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 function makeBoard() {
-  let board = new Array(HEIGHT).fill(null);
-  let row = new Array(WIDTH).fill(null);
+  board = new Array(HEIGHT).fill(null);
   board.forEach((val, i) => {
-    board[i] = row;
+    board[i] = new Array(WIDTH).fill(null);
   });
 	return board;
 }
@@ -39,7 +39,7 @@ function makeBoard() {
 function makeHtmlBoard() {
   let board = document.querySelector("#board");
   // assemble HTML for top row
-  var top = document.createElement("tr");
+  let top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   // mouseover and mouseout for button UI functionality
   top.addEventListener("mouseover", function(e) {
@@ -51,17 +51,17 @@ function makeHtmlBoard() {
   top.addEventListener("click", handleClick);
   // add data cells to top row
   // 0 1 2 3 4 5 6
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
+  for (let x = 0; x < WIDTH; x++) {
+    let headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
   // append top row to board
   board.append(top);
   // assemble and append the rest of the rows
-  for (var y = 0; y < HEIGHT; y++) {
+  for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
@@ -92,6 +92,11 @@ function findSpotForCol(x) {
 /** placeInTable: update DOM to place piece into HTML board */
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+  let targetCell = document.getElementById(`${y}-${x}`);
+  let piece = document.createElement("div");
+  piece.classList.add("piece");
+  piece.classList.add(`p${currPlayer}`);
+  targetCell.appendChild(piece);
 }
 
 
@@ -106,22 +111,33 @@ function endGame(msg) {
 
 /********************************************************************************/
 
+function fillSpot(y, x) {
+  board[y][x] = currPlayer;
+}
+
+function changePlayer() {
+  currPlayer = currPlayer === 1 ? 2 : 1;
+}
+
 
 /** handleClick: handle click of column top to play piece */
 function handleClick(evt) {
-  console.log(evt.target);
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  let x = +evt.target.id;
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
   // if we try to click on a row that's completely full, nothing happens
   if (y === null) {
     return;
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
   placeInTable(y, x);
+  // change appropriate value in matrix from null to player number
+  fillSpot(y, x);
+  //
+  changePlayer();
+
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
@@ -160,12 +176,12 @@ function checkForWin() {
 
 
   // TODO: read and understand this code. Add comments to help you.
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      let diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
